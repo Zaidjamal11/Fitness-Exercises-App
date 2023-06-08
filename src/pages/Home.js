2b7af905e31161cb55fd462df13ca300f25cd1ab
unknown <zaidj777@gmail.com>
2023-06-08 20:35:32 +0530

    Header Banner Feature Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
+
+import { Box } from '@mui/material';
+
+//components
+import Excersises from '../components/Excersises';
+import SearchExcercises from '../components/SearchExcercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+
+    <HeroBanner />
+    <SearchExcercises />
+    <Excersises />
+
+    </Box>
   )
 }
 
