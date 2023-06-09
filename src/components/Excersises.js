caa679ef00d3650dba427223d1450f0ceccd6df9
unknown <zaidj777@gmail.com>
2023-06-10 00:34:16 +0530

    Home page completed

--- src/components/Excersises.js
+++ src/components/Excersises.js
@@ -13,7 +13,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   
 
   // Pagination
-  
+
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
   const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
@@ -23,6 +23,27 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
     window.scrollTo({ top: 1800, behavior: 'smooth' });
   };
+
+
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
+
+
 
   
 
