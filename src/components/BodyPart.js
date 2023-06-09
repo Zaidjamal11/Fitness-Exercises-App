73ad8da1318f12ba46d1aae3fb6a14065583b8f2
unknown <zaidj777@gmail.com>
2023-06-09 23:16:45 +0530

    HorizontalScrollBar feature is compleated

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import {Stack, Typography }from '@mui/material';
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item , BodyPart, setBodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+     type="button"
+     alignItems='center'
+     justifyContent='center'
+     className='bodyPart-card'
+     sx={{
+      borderTop: BodyPart === item ? '4p solid #ff2625' : '',
+      backgroundColor: '#fff',
+      borderBottomLeftRadius: '20px',
+      width: '270px',
+      height: '280px',
+      cursor: 'pointer',
+      gap: '47px'
+  
+
+
+     }}
+    
+    >
+      <img src={Icon} alt='dumbell' style={{ width: '40px', height: '40px'}} />
+    </Stack>
   )
 }
 
